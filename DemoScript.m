55d5c688fe0b2a6f70c71fdbbb87a4086423f750
José P. Marques <j.marques@donders.ru.nl>
2016-10-05 11:03:40 +0200

    Add files via upload

--- DemoScript.m
+++ DemoScript.m
@@ -5,18 +5,21 @@
 file{1}.name='fMRI_rest_cmrr_mb8.nii';
 file{1}.TR=735;
 file{1}.TE=39;
+file{1}.T2star=45;
 file{1}.res=[2.4 2.4 2.4];
 file{1}.outputname='MB8';
 %% protocol 2 parameters
 file{2}.name='fMRI_rest_cmrr_mb8.nii';
 file{2}.TR=600;
 file{2}.TE=33;
+file{2}.T2star=45;
 file{2}.res=[2.4 2.4 2.4];
 file{2}.outputname='MB8short';
 %% protocol 3 parametners
 
-%% aantomical image of subject or some MNI T1w
+%% anatomical image of subject or some MNI T1w
 AnatomicalT1w='t1_mprage_sag_p2_iso_1_0'
+
 system(['bet ',AnatomicalT1w,' brain']);
 
 fslviewdisplay=['fslview brain '];
